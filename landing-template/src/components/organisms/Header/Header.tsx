e8385266c85930679fc751a90fd58086f7870284
Valeria <valleriademelo@gmail.com>
2024-12-03 12:57:37 -0200

    fix: ensure Header.tsx has a proper default export and implementation

---
+++ landing-template/src/components/organisms/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Header.module.css';
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <h1>
+        My Lading page
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
